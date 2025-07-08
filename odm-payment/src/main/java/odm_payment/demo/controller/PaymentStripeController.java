04931fc0081102859b6af188212aca2480c0c3e3
moetaz22 <ghezaielmoetaz@gmail.com>
2025-07-08 13:46:57 +0100

    add stripe treatment

---
+++ odm-payment/src/main/java/odm_payment/demo/controller/PaymentStripeController.java
@@ -0,0 +1,45 @@
+package odm_payment.demo.controller;
+
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
+import odm_payment.demo.dto.PaymentRequest;
+import odm_payment.demo.dto.PaymentResponse;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/payment")
+public class PaymentStripeController {
+
+    @PostMapping("/pay")
+    public PaymentResponse createCheckoutSession(@RequestBody PaymentRequest request,
+                                        @RequestHeader String userEmail) throws StripeException {
+        SessionCreateParams params =
+                SessionCreateParams.builder()
+                        .setMode(SessionCreateParams.Mode.PAYMENT)
+                        .setSuccessUrl("https://www.google.com/")
+                        .setCancelUrl("https://www.youtube.com/")
+                        .addLineItem(
+                                SessionCreateParams.LineItem.builder()
+                                        .setQuantity(1L)
+                                        .setPriceData(
+                                                SessionCreateParams.LineItem.PriceData.builder()
+                                                        .setCurrency("usd")
+                                                        .setUnitAmount(request.getAmount() * 100L)
+                                                        .setProductData(
+                                                                SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                                                        .setName("Service Payment")
+                                                                        .build()
+                                                        )
+                                                        .build()
+                                        )
+                                        .build()
+                        )
+                        .setCustomerEmail(userEmail)
+                        .build();
+
+        Session session = Session.create(params);
+
+        return new PaymentResponse(session.getUrl());
+    }
+}
---
+++ odm-payment/src/main/java/odm_payment/demo/dto/PaymentRequest.java
@@ -0,0 +1,13 @@
+package odm_payment.demo.dto;
+
+public class PaymentRequest {
+    private long amount;
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+}
