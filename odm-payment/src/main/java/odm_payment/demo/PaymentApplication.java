d34766335fc9640eda270317e2321a57b318559b
moetaz22 <ghezaielmoetaz@gmail.com>
2025-07-08 16:58:09 +0100

    create versionLocal

--- odm-payment/src/main/java/odm_payment/demo/PaymentApplication.java
+++ odm-payment/src/main/java/odm_payment/demo/PaymentApplication.java
@@ -2,14 +2,12 @@ package odm_payment.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class gatewayApplication {
+public class PaymentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(gatewayApplication.class, args);
+		SpringApplication.run(PaymentApplication.class, args);
 	}
 
 }
